93c796e82f009a88dd812d45da1d90c9494bf2c1
Chandrashekhar R <cr2007@hw.ac.uk>
2023-07-30 09:16:51 +0000

    Changes script to check duplicate version names
    And also adds colors to the Error, Success messages

--- create_tag.sh
+++ create_tag.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-# Check if the script is called with the tag name parameter
-if [ -z "$1" ]; then
-  echo "Error: Tag name parameter is missing."
-  echo "Usage: $0 <tag-name>"
-  exit 1
-fi
+# List all previous Git tags
+echo "Previous Git tags:"
+git tag
 
-tag_name="$1"
+# Function to prompt the user for tag name
+function get_tag_name {
+  read -p "Enter the tag name: " tag_name
+
+  # Check if the tag name is empty
+  if [ -z "$tag_name" ]; then
+    echo -e "\e[31;1mError:\e[0m Tag name cannot be empty."
+    get_tag_name
+  fi
+}
+
+# Prompt the user for tag name
+get_tag_name
+
+# Check if the tag already exists
+while git tag | grep -q "^$tag_name$"; do
+  echo -e "\e[31;1mError:\e[0m Tag '$tag_name' already exists."
+  get_tag_name
+done
 
 # Run the command to create the tag
 git tag "$tag_name"
 
 # Push the tag to the remote repository
 git push origin "$tag_name"
+
+echo -e "\e[32mTag '$tag_name' created and pushed successfully.\e[0m"
