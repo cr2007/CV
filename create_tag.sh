93c9b5f02acb413346fdcba212c4523a15571127
Chandrashekhar R <cr2007@hw.ac.uk>
2023-08-02 13:18:21 +0000

    Updates script to allow tagging a specific commit

--- create_tag.sh
+++ create_tag.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# List all previous Git tags
-echo "Previous Git tags:"
-git tag
+# Function to list all previous Git tags
+function list_previous_tags {
+  echo "Previous Git tags:"
+  git tag
+}
+
+# Function to prompt the user for commit hash
+function get_commit_hash {
+  echo -e -n "Enter the \e[1mcommit hash\e[0m you want to tag\n(If you want to enter a shortened one, enter the first 7 digits of the commit hash)\n"
+  read commit_hash
+
+  # Check if the commit hash is empty
+  if [ -z "$commit_hash" ]; then
+    echo "Error: Commit hash cannot be empty."
+    get_commit_hash
+  fi
+}
 
 # Function to prompt the user for tag name
 function get_tag_name {
@@ -15,17 +29,59 @@ function get_tag_name {
   fi
 }
 
-# Prompt the user for tag name
-get_tag_name
+# Check if the script is called with the -c flag
+if [[ "$1" == "-c" ]]; then
+  # Prompt the user for commit hash
+  get_commit_hash
 
-# Check if the tag already exists
-while git tag | grep -q "^$tag_name$"; do
-  echo -e "\e[31;1mError:\e[0m Tag '$tag_name' already exists."
+  # Verify if the commit hash exists using grep
+  if git log --pretty=oneline | cut -d' ' -f1 | grep -q "^$commit_hash$"; then
+    echo -e "Commit hash \e[1;33m'$commit_hash'\e[0m exists in the repository."
+  else
+    # Checks with the first 7 digits of the commit hash
+    echo -e "\e[31;1mError:\e[0m Commit hash '$commit_hash' does not exist."
+    echo -e "\e[1;33mRe-checking with first 7 digits of commit hash\e[0m"
+
+	if git log --pretty=oneline | cut -d' ' -f1 | grep -E -q "^${commit_hash:0:7}"; then
+      echo -e "Commit hash \e[1;33m'$commit_hash'\e[0m exists in the repository."
+    else
+      echo -e "\e[31;1mError:\e[0m Commit hash '$commit_hash' does not exist."
+      exit 1
+    fi
+  fi
+
+  # List all previous Git tags
+  list_previous_tags
+
+  # Prompt the user for tag name
   get_tag_name
-done
 
-# Run the command to create the tag
-git tag "$tag_name"
+  # Check if the tag already exists
+  while git tag | grep -q "^$tag_name$"; do
+    echo -e "\e[31;1mError:\e[0m Tag '$tag_name' already exists."
+    get_tag_name
+  done
+
+  # Run the command to create the tag for the specified commit
+  git tag "$tag_name" "$commit_hash"
+
+else
+  # List all previous Git tags
+  list_previous_tags
+
+  # Prompt the user for tag name
+  get_tag_name
+
+  # Check if the tag already exists
+  while git tag | grep -q "^$tag_name$"; do
+    echo -e "\e[31;1mError:\e[0m Tag '$tag_name' already exists."
+    get_tag_name
+  done
+
+  # Run the command to create the tag for the latest commit
+  git tag "$tag_name"
+
+fi
 
 # Push the tag to the remote repository
 git push origin "$tag_name"
